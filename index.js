1cc322c2255fbaea812a49ee8173db9b1f358fea
joannaberlin <joanna.kot.ok@gmail.com>
2023-07-31 18:45:06 +0200

    App redesign

---
+++ index.js
@@ -0,0 +1,2 @@
+import './src/scrollAnimation.js';
+import './src/cursor.js';
---
+++ src/cursor.js
@@ -0,0 +1,44 @@
+var cursor = document.querySelector('.cursor'),
+	follower = document.querySelector('.cursor-follower');
+
+var posX = 0,
+	posY = 0;
+
+var mouseX = 0,
+	mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+	repeat: -1,
+	onRepeat: function () {
+		posX += (mouseX - posX) / 9;
+		posY += (mouseY - posY) / 9;
+
+		TweenMax.set(follower, {
+			css: {
+				left: posX - 12,
+				top: posY - 12,
+			},
+		});
+
+		TweenMax.set(cursor, {
+			css: {
+				left: mouseX,
+				top: mouseY,
+			},
+		});
+	},
+});
+
+document.addEventListener('mousemove', function (e) {
+	mouseX = e.clientX;
+	mouseY = e.clientY;
+});
+
+// document.querySelector('.link').on('mouseenter', function () {
+// 	cursor.addClass('active');
+// 	follower.addClass('active');
+// });
+// document.querySelector('.link').on('mouseleave', function () {
+// 	cursor.removeClass('active');
+// 	follower.removeClass('active');
+// });
