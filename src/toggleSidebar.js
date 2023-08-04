1732d6abf957a5275dedfd02d0ad591fe84a3ed9
joannaberlin <joanna.kot.ok@gmail.com>
2023-08-04 12:25:16 +0200

    Add color version to cursor

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,10 +1,13 @@
 const toggleNav = document.querySelector('.toggle-nav');
 const sidebarOverlay = document.querySelector('.sidebar-overlay');
 const closeBtn = document.querySelector('.sidebar-close');
+const cursorInside = document.querySelector('.cursor');
 
 toggleNav.addEventListener('click', () => {
 	sidebarOverlay.classList.add('show');
+	cursorInside.classList.add('cursor__version-other');
 });
 closeBtn.addEventListener('click', () => {
 	sidebarOverlay.classList.remove('show');
+	cursorInside.classList.remove('cursor__version-other');
 });
