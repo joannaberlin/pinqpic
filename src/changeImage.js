2e02c2f0647107a9d0944ee9bc8a70cf73a3e3bc
joannaberlin <joanna.kot.ok@gmail.com>
2023-08-15 13:21:49 +0200

    Change design, colors switch, home and about pages

--- src/changeImage.js
+++ src/changeImage.js
@@ -6,27 +6,50 @@ const group1Urls = [
 	'../images/home/Group-1/1-home-small-0-bw.jpg',
 	'../images/home/Group-1/1-home-small-4-bw.jpg',
 ];
+const group2Urls = [
+	'../images/home/Group-2/2-home-small-1-bw.jpg',
+	'../images/home/Group-2/2-home-small-2-bw.jpg',
+	'../images/home/Group-2/2-home-small-3-bw.jpg',
+	'../images/home/Group-2/2-home-small-0-bw.jpg',
+	'../images/home/Group-2/2-home-small-4-bw.jpg',
+];
+const group3Urls = [
+	'../images/home/Group-3/3-home-small-1-bw.jpg',
+	'../images/home/Group-3/3-home-small-2-bw.jpg',
+	'../images/home/Group-3/3-home-small-3-bw.jpg',
+	'../images/home/Group-3/3-home-small-4-bw.jpg',
+	'../images/home/Group-3/3-home-small-5-bw.jpg',
+	'../images/home/Group-3/3-home-small-0-bw.jpg',
+	'../images/home/Group-3/3-home-small-6-bw.jpg',
+];
 const group4Urls = [
 	'../images/home/Group-4/4-home-small-1-bw.jpg',
+	'../images/home/Group-4/4-home-small-2-bw.jpg',
 	'../images/home/Group-4/4-home-small-3-bw.jpg',
 	'../images/home/Group-4/4-home-small-4-bw.jpg',
-	'../images/home/Group-4/4-home-small-5-bw.jpg',
-	'../images/home/Group-4/4-home-small-6-bw.jpg',
 	'../images/home/Group-4/4-home-small-0-bw.jpg',
-	'../images/home/Group-4/4-home-small-7-bw.jpg',
+	'../images/home/Group-4/4-home-small-5-bw.jpg',
+];
+const group5Urls = [
+	'../images/home/Group-5/5-home-small-1-bw.jpg',
+	'../images/home/Group-5/5-home-small-2-bw.jpg',
+	'../images/home/Group-5/5-home-small-3-bw.jpg',
+	'../images/home/Group-5/5-home-small-0-bw.jpg',
+	'../images/home/Group-5/5-home-small-4-bw.jpg',
 ];
 
-console.log(window.innerWidth);
-
 const group1Wrapper = document.getElementById('img-1');
+const group2Wrapper = document.getElementById('img-2');
+const group3Wrapper = document.getElementById('img-3');
 const group4Wrapper = document.getElementById('img-4');
+const group5Wrapper = document.getElementById('img-5');
 const time = 500;
 const currentWindowWidth = window.innerWidth;
 let timeoutId;
 let timeoutIdLoop;
 
 const changeImage = (images, container, step) => {
-	if (currentWindowWidth > 1024) {
+	if (currentWindowWidth > 576) {
 		images.forEach(
 			(image, index) =>
 				(timeoutId = setTimeout(() => {
@@ -41,8 +64,10 @@ const changeImage = (images, container, step) => {
 };
 
 const stopChangingImage = () => {
-	clearTimeout(timeoutId);
-	clearTimeout(timeoutIdLoop);
+	if (currentWindowWidth > 576) {
+		clearTimeout(timeoutId);
+		clearTimeout(timeoutIdLoop);
+	}
 };
 
 group1Wrapper.addEventListener('mouseover', () => {
@@ -51,12 +76,27 @@ group1Wrapper.addEventListener('mouseover', () => {
 group1Wrapper.addEventListener('mouseleave', () => {
 	stopChangingImage();
 });
+group2Wrapper.addEventListener('mouseover', () => {
+	changeImage(group2Urls, group2Wrapper, time);
+});
+group2Wrapper.addEventListener('mouseleave', () => {
+	stopChangingImage();
+});
+group3Wrapper.addEventListener('mouseover', () => {
+	changeImage(group3Urls, group3Wrapper, time);
+});
+group3Wrapper.addEventListener('mouseleave', () => {
+	stopChangingImage();
+});
 group4Wrapper.addEventListener('mouseover', () => {
 	changeImage(group4Urls, group4Wrapper, time);
 });
 group4Wrapper.addEventListener('mouseleave', () => {
 	stopChangingImage();
 });
-
-// changeImage(group1Urls, group1Wrapper, time);
-// changeImage(group4Urls, group4Wrapper, time);
+group5Wrapper.addEventListener('mouseover', () => {
+	changeImage(group5Urls, group5Wrapper, time);
+});
+group5Wrapper.addEventListener('mouseleave', () => {
+	stopChangingImage();
+});
--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -2,15 +2,18 @@ const toggleNav = document.querySelector('.toggle-nav');
 const sidebarOverlay = document.querySelector('.sidebar-overlay');
 const closeBtn = document.querySelector('.sidebar-close');
 const cursorInside = document.querySelector('.cursor');
-const footer = document.querySelector('.footer__version');
+const footer = document.querySelector('.footer');
+const nav = document.querySelector('#main-nav');
 
 toggleNav.addEventListener('click', () => {
 	sidebarOverlay.classList.add('show');
 	cursorInside.classList.add('cursor__version-other');
 	footer.classList.add('hide');
+	nav.classList.add('hide');
 });
 closeBtn.addEventListener('click', () => {
 	sidebarOverlay.classList.remove('show');
 	cursorInside.classList.remove('cursor__version-other');
 	footer.classList.remove('hide');
+	nav.classList.remove('hide');
 });
