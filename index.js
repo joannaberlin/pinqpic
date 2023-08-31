a3e6779817f7bcfe41d5ca43275e156df0b0f51b
joannaberlin <joanna.kot.ok@gmail.com>
2023-08-31 16:08:53 +0200

    Test animation in Home Page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import './src/scrollAnimation.js';
 import './src/cursor.js';
 import './src/toggleSidebar.js';
-import './src/changeImage.js';
+// import './src/changeImage.js';
 import './src/loader.js';
+import './src/animateImage.js';
---
+++ src/animateImage.js
@@ -0,0 +1,31 @@
+const group1Urls = [
+	'../images/home/Group-1/1-home-small-5-bw.jpg',
+	'../images/home/Group-1/1-home-small-1-bw.jpg',
+	'../images/home/Group-1/1-home-small-2-bw.jpg',
+	'../images/home/Group-1/1-home-small-3-bw.jpg',
+	'../images/home/Group-1/1-home-small-0-bw.jpg',
+	'../images/home/Group-1/1-home-small-4-bw.jpg',
+];
+
+const group1Wrapper = document.getElementById('img-1');
+let start, previousTimeStamp;
+let done = false;
+const time = 700;
+let timeoutId;
+let timeoutIdLoop;
+
+const changeImage = (images, container, step) => {
+	images.forEach(
+		(image, index) =>
+			(timeoutId = setTimeout(() => {
+				container.style.backgroundImage = `url(${image})`;
+			}, step * (index + 1)))
+	);
+	timeoutIdLoop = setTimeout(
+		() => changeImage(images, container, step),
+		step * images.length
+	);
+	globalID = requestAnimationFrame(changeImage);
+};
+
+changeImage(group1Urls, group1Wrapper, time);
